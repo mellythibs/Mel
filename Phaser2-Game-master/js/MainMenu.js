a9c4ad7330f06b4f63b7fa70dfce700af115fee3
macdaddywong <35041970+macdaddywong@users.noreply.github.com>
2018-05-15 16:51:55 -0400

    Add files via upload
    Uploading these for a student to compare

---
+++ Phaser2-Game-master/js/MainMenu.js
@@ -0,0 +1,33 @@
+Game.MainMenu = function(game){
+
+};
+
+var start;
+
+Game.MainMenu.prototype  = {
+	preload: function(){
+		this.load.spritesheet('start', 'assets/start.png');
+	},
+
+	create: function(){
+		this.stage.backgroundColor = "#4488AA";
+        button = game.add.button(game.world.centerX, game.world.centerY, 'start', actionOnClick, this, 1, 0, 2); //Not sure if it works perfectly yet
+        button.anchor.setTo(0.5,0.5);
+	},
+
+	update: function(){
+	},
+
+	calls: function(){
+		console.log(this);
+		if(OC == true)
+		{this.state.start('OC');}
+		else	
+		{this.state.start('map');}
+	},
+};
+
+//var OC = false;  // temp. boolean statement 
+
+
+//function start()
